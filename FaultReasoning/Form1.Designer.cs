e2b99a21feaeff446e7d4f284118a4fd30d2629c
lihongfei <2278200244@qq.com>
2018-04-07 13:33:06 +0800

    first commit

---
+++ FaultReasoning/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace FaultReasoning
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataResult = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataResult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.AccessibleName = "";
+            this.dataGrid.AllowUserToAddRows = false;
+            this.dataGrid.AllowUserToDeleteRows = false;
+            this.dataGrid.AllowUserToResizeColumns = false;
+            this.dataGrid.AllowUserToResizeRows = false;
+            this.dataGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Location = new System.Drawing.Point(12, 12);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.RowTemplate.Height = 23;
+            this.dataGrid.Size = new System.Drawing.Size(451, 505);
+            this.dataGrid.TabIndex = 0;
+            this.dataGrid.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.OnValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(492, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "提交";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.OnSubmit);
+            // 
+            // dataResult
+            // 
+            this.dataResult.AllowUserToAddRows = false;
+            this.dataResult.AllowUserToDeleteRows = false;
+            this.dataResult.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataResult.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataResult.Location = new System.Drawing.Point(587, 12);
+            this.dataResult.Name = "dataResult";
+            this.dataResult.ReadOnly = true;
+            this.dataResult.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dataResult.RowTemplate.Height = 23;
+            this.dataResult.Size = new System.Drawing.Size(187, 505);
+            this.dataResult.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(804, 529);
+            this.Controls.Add(this.dataResult);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGrid);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "故障识别";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.OnFormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataResult)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGrid;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataResult;
+    }
+}
+
---
+++ FaultReasoning/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Office.Core;
+using Microsoft.Office.Interop.Excel;
+using System.Reflection;
+
+namespace FaultReasoning
+{
+    public partial class Form1 : Form
+    {
+        System.Data.DataTable dt_symptom = new System.Data.DataTable();    // 故障征兆表 
+        System.Data.DataTable dt_fault = new System.Data.DataTable();    // 故障表 
+        _Workbook book;//电子表格
+        int[,] matrix;//关联度矩阵
+        public Form1()
+        {
+            InitializeComponent();
+            try
+            {
+                //启动Excel应用程序
+                Microsoft.Office.Interop.Excel.Application xls = new Microsoft.Office.Interop.Excel.Application();
+                //    _Workbook book = xls.Workbooks.Add(Missing.Value); //创建一张表，一张表可以包含多个sheet
+
+                //如果表已经存在，可以用下面的命令打开
+                book = xls.Workbooks.Open(System.Environment.CurrentDirectory + @"\content.xlsx");
+
+                _Worksheet sheet;//定义sheet变量
+                xls.Visible = false;//设置Excel后台运行
+                xls.DisplayAlerts = false;//设置不显示确认修改提示
+                sheet = (_Worksheet)book.Worksheets.get_Item(1);//获得第i个sheet，准备写入
+
+                dt_symptom.Columns.Add(new DataColumn("isChecked", typeof(bool)));
+                dt_symptom.Columns.Add(new DataColumn("序号", typeof(int)));
+                dt_symptom.Columns.Add(new DataColumn("故障征兆", typeof(string)));
+                dt_fault.Columns.Add(new DataColumn("序号", typeof(int)));
+                dt_fault.Columns.Add(new DataColumn("故障", typeof(string)));
+                //填充故障征兆表
+                for (int index = 1; index < 300; index++)
+                {
+                    Range temp_range = sheet.Cells[index + 1, 1];
+                    if (temp_range.Value == null)
+                        break;
+                    else
+                        dt_symptom.Rows.Add(false, index, temp_range.Value);
+                }
+                //填充故障表
+                for (int index = 1; index < 300; index++)
+                {
+                    Range temp_range = sheet.Cells[1, index + 1];
+                    if (temp_range.Value == null)
+                        break;
+                    else
+                        dt_fault.Rows.Add(index, temp_range.Value);
+                }
+
+                //填充规则矩阵
+                matrix = new int[dt_symptom.Rows.Count, dt_fault.Rows.Count];
+                for (int i = 0; i < dt_symptom.Rows.Count; i++)
+                    for (int j = 0; j < dt_fault.Rows.Count; j++)
+                    {
+                        Range temp_range = sheet.Cells[i + 2, j + 2];
+                        matrix[i, j] = Convert.ToInt32(temp_range.Value);
+                    }
+                dataGrid.DataSource = dt_symptom;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void OnValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void OnSubmit(object sender, EventArgs e)//提交代码处理
+        {
+            try
+            {
+                System.Data.DataTable temp_dt = dt_fault.Copy();
+                for (int index = 0; index < dt_fault.Rows.Count; index++)
+                {
+                    string str_fault = dt_fault.Rows[index]["故障"] as string;
+                    for (int i = 0; i < dt_symptom.Rows.Count; i++)
+                    {
+                        if (matrix[i, index] == 1)//规则1必要性检测
+                        {
+                            if ((bool)dt_symptom.Rows[i]["isChecked"] == false)
+                            {
+                                DataRow[] rows = temp_dt.Select("故障='" + str_fault + "'");
+                                if (rows.Count() == 1)
+                                {
+                                    temp_dt.Rows.Remove(rows[0]);
+                                    continue;
+                                }
+                                else if (rows.Count() > 1)
+                                {
+                                    throw new Exception("故障名称重复");
+                                }
+                            }
+                        }
+                        if (matrix[i, index] == -1)//规则3充分性检测
+                        {
+                            if ((bool)dt_symptom.Rows[i]["isChecked"] == true)
+                            {
+                                DataRow[] rows = temp_dt.Select("故障='" + str_fault + "'");
+                                if (rows.Count() == 1)
+                                {
+                                    temp_dt.Rows.Remove(rows[0]);
+                                    continue;
+                                }
+                                else if (rows.Count() > 1)
+                                {
+                                    throw new Exception("故障名称重复");
+                                }
+                            }
+                        }
+                    }
+                }
+                dataResult.DataSource = temp_dt;
+                // temp_dt.Rows.
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void OnFormClosed(object sender, FormClosedEventArgs e)
+        {
+            try
+            {
+                book.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
